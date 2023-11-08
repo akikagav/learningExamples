14b128f5dd42c56158ec0a85938174ebe0e53a7b
Astghik <akikagav@gmail.com>
2023-11-08 18:33:51 +0400

    chore: Initial project

---
+++ src/main/java/com/example/learningExamples/LearningExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learningExamples/LearningExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningExamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
